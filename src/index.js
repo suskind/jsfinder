1a7540dc560af38abbbd169aec1a0f19c2b461f7
Claudio Gamboa <gamboa@pdvel.com>
2020-02-21 03:05:19 +0000

    first complete release

--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
-
+const fs = require('fs');
+const crypto = require('crypto');
+const { exec } = require('child_process');
 const puppeteer = require('puppeteer');
 const yargs = require('yargs');
+const cliProgress = require('cli-progress');
+// const loading =  require('loading-cli');
 // const program = require('commander');
 
-const beautify = require('js-beautify');
+const iPhoneMobile = puppeteer.devices['iPhone X'];
 
-const { autoScroll, isAdsURL } = require('./utils');
+const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+
+const { chromeConfig } = require('./config');
+const {
+  autoScroll,
+  isAdsURL,
+  writeOutputTerminal,
+  createOutputDir,
+  prettyFileAndLog,
+} = require("./utils");
  
 (async () => {
 
-  const aURLs = [];
+  let aURLs = [];
 
   const argv = yargs.command('jsfinder', 'Parse JS files for URLs and stuff', {
-    // url: {
-    //   description: 'URL to be parsed',
-    //   alias: 'u',
-    //   type: 'string',
-    // }
   }).option('url', {
     description: 'URL to be parsed',
     alias: 'u',
+    type: 'string',
+  }).option('output', {
+    description: 'Output dir',
+    alias: 'o',
     type: 'string',
   }).option('header', {
     description: 'Header',
@@ -28,43 +40,43 @@ const { autoScroll, isAdsURL } = require('./utils');
   }).option('browser', {
     description: 'Open browser',
     type: 'boolean',
-  }).option('output', {
-    description: 'Output dir',
-    alias: 'o',
+  }).option('ads', {
+    description: 'Allow blacklisted ads related URLs',
+    type: 'boolean',
+  }).option('insource', {
+    description: 'Search for URLs in source',
+    type: 'boolean',
+  }).option('nocache', {
+    description: 'Ingore cache when it already exists',
+    type: 'boolean',
+  }).option('mobile', {
+    description: 'Emulate Mobile phone (iPhone X)',
     type: 'string',
+    alias: 'm',
+  }).option('vscode', {
+    description: 'Open output dir with VSCode',
+    type: 'boolean',
+  }).option('noscroll', {
+    description: 'Dont do autoscroll',
+    type: 'boolean',
   }).help().alias('help', 'h').argv;
 
-  console.log(argv);
+  // console.log(argv);
 
   if (!argv.url) {
     console.log('Error: required URL :: --url/-u <https://example.com>');
     return;
   }
 
-  // return;
-
   const browser = await puppeteer.launch({
+    ...chromeConfig,
     headless: argv.browser ? false : true,
-    devtools: false,
-    ignoreHTTPSErrors: true,
-    defaultViewport: null,
-    // defaultViewport: {
-    //   width: 1280,
-    //   height: 800
-    // },
-    args: [
-      '--disable-xss-auditor',
-      '--ignore-certificate-errors',
-      '--allow-running-insecure-content',
-      '--disable-web-security',
-      '--disable-resize-lock',
-      '--window-size=1280,800',
-      '--no-sandbox'
-    ]
   });
   let page = null;
   [page] = await browser.pages();
   // const page = await browser.newPage();
+  // await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
+  // await page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:72.0) Gecko/20100101 Firefox/72.0');
 
   await page.setRequestInterception(true);
 
@@ -78,8 +90,10 @@ const { autoScroll, isAdsURL } = require('./utils');
   page.on('response', async res => {
     const url = await res.url();
     
-    if (isAdsURL(url)) {
-      return;
+    if (!argv.ads) {
+      if (isAdsURL(url)) {
+        return;
+      }
     }
     const method = await res.request().method();
     const isDataUrl = url.indexOf('data');
@@ -87,17 +101,20 @@ const { autoScroll, isAdsURL } = require('./utils');
       return;
     }
 
-    const filterType = await res.request().resourceType();
-    // console.log(filterType)
+    let filterType = await res.request().resourceType();
     if (['document', 'script', 'xhr'].indexOf(filterType) === -1) {
       return;
     }
     const status = await res.status();
     const statusText = await res.statusText();
+    const reqHeaders = await res.request().headers();
     const resHeaders = { ...(await res.headers()) };
     const type = 'content-type' in resHeaders
           ? resHeaders['content-type'].split(';')[0]
           : null;
+    if (filterType === 'document' && type && (type.indexOf('javascript') > -1 || type.indexOf('json') > -1)) {
+      filterType = 'document_script';
+    }
     let resBody = '';
     try {
       resBody = await res.text();
@@ -108,46 +125,78 @@ const { autoScroll, isAdsURL } = require('./utils');
     if (!resBody) {
       return;
     }
-
-    let bodyIndent = '';
-    if (filterType === 'document') {
-      bodyIndent = beautify.html(resBody, { indent_size: 2 });
-    } else if (filterType === 'script') {
-      bodyIndent = beautify.js(resBody, { indent_size: 2 });
-    } else if (filterType === 'xhr') {
-      if (type.indexOf('json') > -1 || type.indexOf('javascript') > -1) {
-        bodyIndent = beautify.js(resBody, { indent_size: 2 });
-      } else if (type.indexOf('html')) {
-        bodyIndent = beautify.html(resBody, { indent_size: 2 });
-      } else {
-        bodyIndent = resBody;
-      }
-    }
-
-    aURLs.push({
+    
+    const obj = {
       filterType,
       method,
       status,
       url,
-      // body: bodyIndent,
-    });
-
-    // console.log('#################################');
-    // console.log('--------------------------------');
-    // console.log(url);
-    // console.log('--------------------------------');
-    // console.log(filterType);
+      contentType: type,
+      reqHeaders,
+      resHeaders,
+      body: resBody,
+    };
+    aURLs.push(obj);
+    await writeOutputTerminal(obj);
   });
-  // const browser = await puppeteer.launch();
-  // const page = await browser.newPage();
-  // await page.goto('https://example.com');
- 
-  // await browser.close();
-  await page.goto(argv.url, {waitUntil: 'networkidle2'});
+  
+  let hasCache = false;
+  const hashCache = crypto.createHash('md5').update(argv.url).digest('hex');
+  const cacheFileName = `/tmp/jsfinder_cache_${hashCache}.json`;
+  if (fs.existsSync('/tmp') && fs.existsSync(cacheFileName)) {
+    hasCache = true;
+    if (argv.nocache) {
+      hasCache = false;
+    }
+  }
 
-  await autoScroll(page);
+  if (!hasCache) {
+    console.log('No cache... will open URL....');
+    if (argv.mobile) {
+      await page.emulate(iPhoneMobile);
+    }
+    await page.goto(argv.url, {waitUntil: 'networkidle2'});
 
-  await browser.close();
+    if (!argv.noscroll) {
+      await autoScroll(page);
+    }
 
-  console.log(aURLs);
+    if (!argv.browser) {
+      await browser.close();
+    }
+    console.log('Writing cache file...');
+    fs.writeFileSync(cacheFileName, JSON.stringify(aURLs), 'utf8');
+  } else {
+    console.log('Warning :: Using cached data');
+    const cachedJSON = fs.readFileSync(cacheFileName);
+    aURLs = JSON.parse(cachedJSON);
+  }
+
+  if (argv.output) {
+    console.log('Beautifying resources...');
+
+    progressBar.start(aURLs.length, 0);
+    await createOutputDir(argv.output);
+    
+    for (let i=0; i < aURLs.length; i++) {
+      await prettyFileAndLog(argv.output, aURLs[i], argv.insource);
+      await progressBar.increment();
+    }
+
+    progressBar.stop();
+
+    if (argv.vscode) {
+      exec(`code ${argv.output}`);
+    }
+  }
+  console.log('Done.');
+  if (!argv.browser) {
+    process.exit(0);
+  }
+  // load({
+  //   text: 'Beautifying found resources...',
+  //   frames: ["◐", "◓", "◑", "◒"],
+  // }).start();
+
+  // console.log(aURLs);
 })();
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,10 @@
-module.exports = {
+const fs = require('fs');
+const crypto = require('crypto')
+const beautify = require('js-beautify');
+
+const { adsBlackList } = require('./config');
+
+const modules = {
   autoScroll: async function(page) {
     await page.evaluate(async () => {
       await new Promise((resolve, reject) => {
@@ -20,51 +26,7 @@ module.exports = {
 
 
   isAdsURL: function(url) {
-    const list = [
-      'googletagmanager.com',
-      'google-analytics.com',
-      'googleadservices.com',
-      'redditstatic.com',
-      'ads-twitter.com',
-      'facebook.net',
-      'albacross.com',
-      'google-analytics.com',
-      'hs-analytics.net',
-      'doubleclick.net',
-      'analytics.twitter.com',
-      'intercomcdn.com',
-      'hs-scripts.com',
-      'usemessages.com',
-      'snap.licdn.com',
-      'consensu.org',
-      'gemius.pl',
-      'pubstack.io',
-      'bkrtx.com',
-      'jsdelivr.net',
-      'bluekai.com',
-      'digitru.st',
-      'rubiconproject.com',
-      'criteo.net',
-      'adnxs.com',
-      'pub.web.sapo.io',
-      'taboola.com',
-      'hotjar.com',
-      'scorecardresearch.com',
-      'moatads.com',
-      '2mdn.net',
-      'adservice.google.pt',
-      'adservice.google.com',
-      'googletagservices.com',
-      'navdmp.com',
-      'googlesyndication.com',
-      'facebook.com/plugins/like',
-      'facebook.com/rsrc.php',
-      'sdk.privacy-center.org',
-      'amazon-adsystem.com',
-      'outbrain.com',
-      'onetrust.com',
-      'cdn.cookielaw.org',
-    ];
+    const list = [].concat(adsBlackList);
     for (let i=0; i < list.length; i++) {
       if (url.indexOf(list[i]) > -1) {
         return true;
@@ -73,5 +35,186 @@ module.exports = {
     return false;
   },
 
+  beautify: function(filterType, contentType, resBody) {
+    let bodyIndent = '';
+    if (filterType === 'document') {
+      bodyIndent = beautify.html(resBody, { indent_size: 2 });
+    } else if (filterType === 'script' || filterType === 'document_script') {
+      bodyIndent = beautify.js(resBody, { indent_size: 2 });
+    } else if (filterType === 'xhr') {
+      if (contentType.indexOf('json') > -1 || contentType.indexOf('javascript') > -1) {
+        bodyIndent = beautify.js(resBody, { indent_size: 2 });
+      } else if (contentType.indexOf('html')) {
+        bodyIndent = beautify.html(resBody, { indent_size: 2 });
+      } else {
+        bodyIndent = resBody;
+      }
+    }
+    return bodyIndent;
+  },
+
+  writeOutputTerminal: function(obj) {
+    const { status, contentType, filterType, url } = obj;
+    console.log(`${filterType} :: (${status}) ${contentType} :: ${url}`);
+  },
+
+  createOutputDir: function(dir) {
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+  },
+
+  formatHeders: function(obj, direction) {
+    let sign = '';
+    if (direction === 'req') {
+      sign = '>';
+    } else if (direction) {
+      sign = '<';
+    }
+    let str = '';
+    for (let prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        str += `${sign} ${prop}: ${obj[prop]}\n`;
+      }
+    }
+    return str;
+  },
+
+  prettyFileAndLog: async function(dir, obj, fetchInSource) {
+    const { contentType, filterType, reqHeaders, resHeaders, url, body, status } = obj;
+    let extension = '.js';
+    let headers = '';
+    if (filterType === 'document') {
+      extension = '.html';
+      headers = `
+<!--
+${url}
+
+${modules.formatHeders(reqHeaders, 'req')}
+
+${modules.formatHeders(resHeaders, 'res')}
+
+-->
+      `;
+    } else if (filterType === 'script' || filterType === 'document_script') {
+      extension = '.js';
+      headers = `
+/*
+${url}
+
+${modules.formatHeders(reqHeaders, 'req')}
+
+${modules.formatHeders(resHeaders, 'res')}
+
+*/
+      `;
+    } else if (filterType === 'xhr') {
+      if (contentType.indexOf('json') > -1 || contentType.indexOf('javascript') > -1) {
+        extension = '.json';
+        headers = `
+/*
+${url}
+
+${modules.formatHeders(reqHeaders, 'req')}
+
+${modules.formatHeders(resHeaders, 'res')}
+
+*/
+        `;
+      } else if (contentType.indexOf('html')) {
+        extension = '.html';
+        headers = `
+<!--
+${url}
+
+${modules.formatHeders(reqHeaders, 'req')}
+
+${modules.formatHeders(resHeaders, 'res')}
+
+-->
+        `;
+      } else {
+        extension = '.txt';
+        headers = `
+/*
+${url}
+
+${modules.formatHeders(reqHeaders, 'req')}
+
+${modules.formatHeders(resHeaders, 'res')}
+
+*/
+        `;
+      }
+    }
+    let out = '';
+    if (body) {
+      const prettyBody = modules.beautify(filterType, contentType, body);
+      out = `${headers}
+${prettyBody}
+      `;
+
+      modules.writeBodyAndLog({dir, status, url, body, out, extension, filterType, contentType});
+      if (fetchInSource) {
+        modules.searchUrlInSourceAndLog({dir, url, prettyBody, out, extension, filterType, contentType});
+      }
+      return true;
+    }
+
+    return null;
+  },
+
+  writeBodyAndLog: function(obj) {
+    const { dir, url, status, body, out, extension, filterType, contentType } = obj;
+
+    const hash = crypto.createHash('md5').update(body).digest('hex');
+    const fileName = `${hash}${extension}`;
+    fs.writeFileSync(`${dir}/${fileName}`, out, 'utf8');
+    fs.writeFileSync(`${dir}/index`, `${dir}/${fileName} :: ${url} :: [${status}] (${filterType}) :: (${contentType})\n`, {encoding: 'utf8', flag: 'a'});
+    fs.writeFileSync(`${dir}/index_paths.md`, `## ${url}\n#### ${dir}/[${fileName}](./${fileName})\n##### \`[${status}] (${filterType}) - (${contentType})\`\n`, {encoding: 'utf8', flag: 'a'});
+  
+    return true;
+  },
+
+  searchUrlInSourceAndLog: function(obj) {
+    const { dir, url, prettyBody, out, extension, filterType, contentType } = obj;
+
+    const aBody = prettyBody.split('\n');
+    const aLinks = [];
+    let toFind = false;
+    try {
+      aBody.forEach((line) => {
+        if (filterType !== 'document') {
+          toFind = true;
+        }
+        if (filterType === 'document' && toFind && line.indexOf('</script>') > -1) {
+          toFind = false;
+        }
+        if (filterType === 'document' && line.indexOf('<script') > -1 && line.indexOf('src=') === -1) {
+          toFind = true;
+        }
+        // console.log('toFind :: ', toFind)
+        if (toFind) {
+          const reg = new RegExp("[^/][`'\"]([\/][a-zA-Z0-9_.-]+)+(?!([gimuy]*[,;\s])|\/\2)");
+          const aMatch = line.match(reg);
+          // console.log(aMatch)
+          if (aMatch) {
+            const path = aMatch[0].replace(/^[^\/]+/g, '');
+            aLinks.push(path);
+            fs.writeFileSync(`${dir}/index_paths.md`, ` * \`${path}\` \n`, {encoding: 'utf8', flag: 'a'});
+            fs.writeFileSync(`${dir}/index_paths.md`, `\`\`\`js\n${aMatch.input.trim()}\n\`\`\`\n`, {encoding: 'utf8', flag: 'a'});
+          }
+        }
+        toFind = false;
+      });
+    } catch(matchEx) {
+      console.log('Error on match :: ', matchEx);
+    }
+    return true;
+  }
+
+
 
 };
+
+module.exports = modules;
